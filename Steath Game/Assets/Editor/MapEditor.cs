54defd1683308dd93bc057ef2b7af312bbd37bb3
Arifa tyrell <arifa.tyrell@gmail.com>
2019-11-12 14:18:00 -0500

    added map gen
    added more folder and organzine project

---
+++ Steath Game/Assets/Editor/MapEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor (typeof (MapGen))]
+public class MapEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        MapGen map = target as MapGen;
+        map.GenrateMap();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

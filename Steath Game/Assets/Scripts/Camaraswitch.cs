472b1afab8d6e37ddbfb9f466f83d97bd16822a7
Arifa tyrell <arifa.tyrell@gmail.com>
2019-12-05 04:21:58 -0500

    update added ai and new charater
    added ai to the other scence
    
    added new character that uses pathfollowing ai without shooting

---
+++ Steath Game/Assets/Scripts/Camaraswitch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camaraswitch : MonoBehaviour
+{
+
+    public GameObject camoneThird;
+    public GameObject camtwoTop;
+
+
+    AudioListener camaudio1;
+    AudioListener camaudio2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camaudio1 = camoneThird.GetComponent<AudioListener>();
+        camaudio2 = camtwoTop.GetComponent<AudioListener>();
+
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchCam();   
+    }
+
+    void switchCam()
+    {
+      
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            camoneThird.SetActive((true));
+            camaudio1.enabled = true;
+
+
+            camtwoTop.SetActive((false));
+            camaudio2.enabled = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            camoneThird.SetActive((false));
+            camaudio1.enabled = false;
+
+
+            camtwoTop.SetActive((true));
+            camaudio2.enabled = true;
+        }
+    }
+}
